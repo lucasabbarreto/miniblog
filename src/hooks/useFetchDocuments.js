5c4e5f05dc19e43546157c505f1306321383e7c0
Lucas Barreto <lucasabbarreto@hotmail.com>
2026-05-05 14:18:30 -0300

    concluindo estrutura de dashboard

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -25,6 +25,8 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
 
                 if (search) {
                     q = await query(collectionRef, where("tagsArray", "array-contains", search), orderBy("createdAt", "desc"));
+                } else if (uid) {
+                    q = await query(collectionRef, where("uid", "==", uid), orderBy("createdAt", "desc"));
                 } else {
                     q = await query(collectionRef, orderBy("createdAt", "desc"));
                 }
@@ -49,7 +51,7 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
         }
         loadData();
 
-    }, [docCollection, documents, search, uid, cancelled])
+    }, [docCollection, search, uid, cancelled])
 
     useEffect(() => {
         return () => setCancelled(true);
